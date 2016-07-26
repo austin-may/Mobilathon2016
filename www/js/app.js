06cae16d3d32977fe8ca15ef8ea420af2c15fabb
Hifza Sakhi <hifzasakhi@users.noreply.github.com>
2016-07-26 17:58:15 -0400

    adding sample template as a state

--- www/js/app.js
+++ www/js/app.js
@@ -58,6 +58,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
+    
+  .state('app.sample', {
+    url: '/sample',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/sample.html'
+      }
+    }
+  })  
 
   .state('app.single', {
     url: '/playlists/:playlistId',
