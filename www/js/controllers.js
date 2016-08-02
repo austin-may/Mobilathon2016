c0ae6dfedfa62d06a6747c9862df0899b3b0cdb0
Anders Olsson <ao685f@att.com>
2016-08-02 11:19:52 -0400

    added functionality for scanning qr codes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -33,27 +33,15 @@ angular.module('starter.controllers', [])
   })
 }])
 
-.controller('RequestsCtrl', function($scope, requests) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.requests = requests.all();
-  $scope.remove = function(request) {
-    requests.remove(request);
-  };
-})
-
-.controller('RequestDetailCtrl', function($scope, $stateParams, requests) {
-  $scope.request = requests.get($stateParams.requestId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('RequestsCtrl', function($scope, $cordovaBarcodeScanner) {
+  $scope.scanBarcode = function() {
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
+            alert(imageData.text);
+            console.log("Barcode Format -> " + imageData.format);
+            console.log("Cancelled -> " + imageData.cancelled);
+        }, function(error) {
+            console.log("An error happened -> " + error);
+        });
+    };
+ 
 });
